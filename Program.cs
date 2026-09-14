c3d0df36e44a899f9c834e08d0d6f25d85797f17
lnulll <rfr.lnulll1111@outlook.com>
2026-09-14 19:24:51 +0300

    Добавлен цветной вывод сообщений

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Красный текст в консоли!");
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Зеленый текст в консоли!");
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Синий текст в консоли!");
+
+        Console.ResetColor(); 
+    }
+}
